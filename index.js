b11a74b2c47a5195673d6da2b4fbe7e2a40a1d35
BLANCA DOMINGO <blaaanca@yahoo.es>
2022-03-21 10:00:52 +0100

    install init express nodemon cors morgan

---
+++ index.js
@@ -0,0 +1,12 @@
+// npm init -y
+// npm install express
+// npm install nodemon
+// npm install cors
+// npm install morgan
+
+// npm install mongoose
+// npm install dotenv
+// npm install @faker-js/faker
+// npm install validatorjs
+// npm install bcrypt
+// npm install jsonwebtoken
