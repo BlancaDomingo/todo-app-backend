51cc7fb63f04db70eb18bb3ea8b18195b4d1c460
BLANCA DOMINGO <blaaanca@yahoo.es>
2022-03-22 10:24:40 +0100

    register User

--- src/controller/users.controller.js
+++ src/controller/users.controller.js
@@ -56,36 +56,6 @@ export const deleteUser = async (req, res) => {
 }
 
 
-export const registerUser = async (req, res) => {
-    let { firstName, lastName, email, password } = req.body;
-    if (
-        !/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/? ]).{8,}$/.test(
-            password
-        )
-    ) {
-        res
-            .status(400)
-            .json({
-                message:'Password should contain number, uppercase, lowercase, special character.'
-            });
-        return;
-    }
-    const checkUser = await User.findOne({ email: email });
-    if (checkUser) {
-        res.status(400).json({message: 'User already exists'});
-        return;
-    }
-    password = await bcrypt.hash(password, 10);
-    try {
-        const role = "user";
-        await User.create({ email, password, firstName, lastName, role });
-
-        res.status(201).send('user created');
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error);
-    }
-}
 
 export const registerAdmin = async (req, res) => {
     let { firstName, lastName, email, password } = req.body;
@@ -97,13 +67,13 @@ export const registerAdmin = async (req, res) => {
         res
             .status(400)
             .json({
-                message:'Password should contain number, uppercase, lowercase, special character.'
+                message: 'Password should contain number, uppercase, lowercase, special character.'
             });
         return;
     }
     const checkUser = await User.findOne({ email: email });
     if (checkUser) {
-        res.status(400).json({message: 'User already exists'});
+        res.status(400).json({ message: 'User already exists' });
         return;
     }
     password = await bcrypt.hash(password, 10);
@@ -115,6 +85,68 @@ export const registerAdmin = async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).send(error);
+    }
+}
+
+
+export const registerUser = async (req, res) => {
+    let { firstName, lastName, email, password } = req.body;
+
+
+    const checkUser = await User.findOne({ email: email });
+    if (checkUser) {
+        res.status(400).json({ message: 'User already exists' });
+        return;
+    }
+
+    if (
+        !/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/? ]).{8,}$/.test(
+            password
+        )
+    ) {
+        res
+            .status(400)
+            .json({
+                message: 'Password should contain number, uppercase, lowercase, special character.'
+            });
+        return;
+    }
+
+    password = await bcrypt.hash(password, 10);
+    /*  try {
+         const role = "user";
+         
+         await User.create({ firstName, lastName, email, password, role });
+ 
+         res.status(201).send('user created');
+     } catch (error) {
+         console.error(error);
+         res.status(500).send(error);
+     } */
+    const role = "user";
+    const userCreated = await User.create({ firstName, lastName, email, password, role });
+
+    if (userCreated) {
+        const tokenData = {
+            userId: User._id,
+            email: User.email,
+
+        }
+
+        const token = jwt.sign(tokenData, process.env.JWT_KEY)
+
+        return res.json({
+            message: 'success',
+            data: {
+                user: User,
+                token: token,
+            }
+        });
+
+    } else {
+        return res.status(500).json({
+            message: 'Fehler bei Konto erstellen!'
+        })
     }
 }
 
@@ -135,9 +167,9 @@ export const loginUser = async (req, res) => {
             const tokenData = {
                 userId: user._id,
                 email: user.email,
-               
+
             }
-           
+
             const token = jwt.sign(tokenData, process.env.JWT_KEY)
 
             return res.json({
@@ -145,7 +177,7 @@ export const loginUser = async (req, res) => {
                 data: {
                     user: user,
                     token: token,
-                      }
+                }
             });
         } else {
             return res.status(402).json({
